66cff11df5d3a83f1a17548a3b4ccc8c06122d1c
Andrei Copelet <andreicop80@gmail.com>
2021-12-10 05:33:41 -0500

    Add files via upload
    Function called describe_city() that accepts the name of a city and its country.

---
+++ cities__using_function.py
@@ -0,0 +1,8 @@
+
+def describe_city(city_name, country):
+    print(f"\n{city_name.title()} is in {country.title()}.")
+
+
+describe_city('chisinau', 'moldova')
+describe_city('montreal', 'canada')
+describe_city('moscow', 'russia')
